022e92cfaed718de80a16f5f42070c1a0902dcb7
waglin <sweet801121@gmail.com>
2021-03-24 16:37:52 +0800

    Java and Kotlin variable, data type

---
+++ src/main/kotlin/com/wag/Hello.kt
@@ -0,0 +1,19 @@
+package com.wag
+
+fun main(args:Array<String>) {
+//    println("Hello kotlin")
+    Human().hello()
+    var age = 19
+    age = 20
+    var weight = 66.5f
+    var name : String
+    name = "wag"
+
+
+}
+
+class Human{
+    fun hello(){
+        println("Hello Kotlin")
+    }
+}
